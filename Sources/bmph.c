5c0a2513ab61c989550cbe4e791c463eeac46922
haramaki[ptr] <49096527+HaramakiPtr@users.noreply.github.com>
2019-03-31 15:20:19 +0900

    Update bmph.c

--- Sources/bmph.c
+++ Sources/bmph.c
@@ -5,30 +5,30 @@
 
 char* getHeaderInfo(int size)
 {
-	char* header = (char*)calloc(sizeof(char),54);
-	
-	header[0] = 'B';
-	header[1] = 'M';
-	
-	int* di;
-	di = (int*)(header + 2);
-	*di = (size * size)*3 + 54;
-	di = (int*)(header + 10);
-	*di = 54;
-	di = (int*)(header + 14);
-	*di = 40;
-	di = (int*)(header + 18);
-	*di = size;
-	di = (int*)(header + 22);
-	*di = size;
-	
-	short* ds;
-	ds = (short*)(header + 26);
-	*ds = 1;
-	ds = (short*)(header + 28);
-	*ds = 24;
-	
-	return header;
+  char* header = (char*)calloc(sizeof(char),54);
+
+  header[0] = 'B';
+  header[1] = 'M';
+
+  int* di;
+  di = (int*)(header + 2);
+  *di = (size * size)*3 + 54;
+  di = (int*)(header + 10);
+  *di = 54;
+  di = (int*)(header + 14);
+  *di = 40;
+  di = (int*)(header + 18);
+  *di = size;
+  di = (int*)(header + 22);
+  *di = size;
+
+  short* ds;
+  ds = (short*)(header + 26);
+  *ds = 1;
+  ds = (short*)(header + 28);
+  *ds = 24;
+
+  return header;
 }
 
 #endif
